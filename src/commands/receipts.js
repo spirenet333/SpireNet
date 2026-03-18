183ed4f7804c0bfd2e7f05e62c447cd94ef77552
spirenet333 <experimental.acs@gmail.com>
2026-03-18 12:49:34 -0700

    Add functions to create and update receipts
    Signed-off-by: spirenet333 <experimental.acs@gmail.com>

--- src/commands/receipts.js
+++ src/commands/receipts.js
@@ -1,1 +1,29 @@
+import { db, saveDB } from "../core/db.js";
+import { generateId } from "../core/utils.js";
 
+export function newReceipt(print) {
+  let id = generateId();
+
+  db.receipts[id] = {
+    id,
+    vendor: "",
+    amount: 0,
+    job: "",
+    category: ""
+  };
+
+  saveDB();
+  print(`receipt created: ${id}`);
+}
+
+export function setReceipt(args, print) {
+  let id = args[0];
+  let field = args[1];
+  let value = args.slice(2).join(" ");
+
+  if (!db.receipts[id]) return print("receipt not found");
+
+  db.receipts[id][field] = value;
+  saveDB();
+  print("receipt updated");
+}
