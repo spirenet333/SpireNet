cfc2d4245d5fec99c9c31a5901d95008fefb8a0e
spirenet333 <experimental.acs@gmail.com>
2026-03-18 16:21:49 -0700

    Add payment creation and update functions
    Signed-off-by: spirenet333 <experimental.acs@gmail.com>

--- src/commands/payments.js
+++ src/commands/payments.js
@@ -1,1 +1,33 @@
+import { db, saveDB } from "../core/db.js";
+import { generateId } from "../core/utils.js";
 
+export function newPayment(print) {
+  let id = generateId();
+
+  db.payments[id] = {
+    id,
+    invoice: "",
+    amount: 0
+  };
+
+  saveDB();
+  print(`payment created: ${id}`);
+}
+
+export function setPayment(args, print) {
+  let id = args[0];
+  let field = args[1];
+  let value = args.slice(2).join(" ");
+
+  if (!db.payments[id]) return print("payment not found");
+
+  db.payments[id][field] = value;
+
+  if (field === "invoice") {
+    let inv = db.invoices[value];
+    if (inv) inv.paid = true;
+  }
+
+  saveDB();
+  print("payment updated");
+}
