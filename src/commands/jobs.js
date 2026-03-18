249568a3437cb8e9835f0831894c4fd037cd4516
spirenet333 <experimental.acs@gmail.com>
2026-03-18 12:49:01 -0700

    Add job management functions for newJob, setJob, and viewJob
    Signed-off-by: spirenet333 <experimental.acs@gmail.com>

--- src/commands/jobs.js
+++ src/commands/jobs.js
@@ -1,1 +1,35 @@
+import { db, saveDB } from "../core/db.js";
+import { generateId } from "../core/utils.js";
 
+export function newJob(print) {
+  let id = generateId();
+
+  db.jobs[id] = {
+    id,
+    customer: "",
+    status: "scheduled",
+    notes: ""
+  };
+
+  saveDB();
+  print(`job created: ${id}`);
+}
+
+export function setJob(args, print) {
+  let id = args[0];
+  let field = args[1];
+  let value = args.slice(2).join(" ");
+
+  if (!db.jobs[id]) return print("job not found");
+
+  db.jobs[id][field] = value;
+  saveDB();
+  print("job updated");
+}
+
+export function viewJob(args, print) {
+  let j = db.jobs[args[0]];
+  if (!j) return print("job not found");
+
+  print(JSON.stringify(j, null, 2));
+}
