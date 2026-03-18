0329666ff362fce4cfb4ed1b03467a272f60c2b4
spirenet333 <experimental.acs@gmail.com>
2026-03-18 12:36:38 -0700

    Implement ledger function to calculate revenue and expenses
    Signed-off-by: spirenet333 <experimental.acs@gmail.com>

--- src/commands/ledger.js
+++ src/commands/ledger.js
@@ -1,1 +1,25 @@
+export function ledger(db, print) {
+  let revenue = 0;
+  let expenses = 0;
 
+  for (let id in db.invoices) {
+    let inv = db.invoices[id];
+    if (inv.paid === true) {
+      revenue += Number(inv.amount || 0);
+    }
+  }
+
+  for (let id in db.receipts) {
+    let r = db.receipts[id];
+    expenses += Number(r.amount || 0);
+  }
+
+  let profit = revenue - expenses;
+
+  print(`
+ledger report:
+revenue: ${revenue}
+expenses: ${expenses}
+profit: ${profit}
+`);
+}
