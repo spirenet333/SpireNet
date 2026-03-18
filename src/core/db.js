6342a531dd2b5362f44ea71973749685d65552d3
spirenet333 <experimental.acs@gmail.com>
2026-03-18 12:40:33 -0700

    Add database object and save/load functions
    Signed-off-by: spirenet333 <experimental.acs@gmail.com>

--- src/core/db.js
+++ src/core/db.js
@@ -1,1 +1,18 @@
+export let db = {
+  customers: {},
+  jobs: {},
+  receipts: {},
+  invoices: {},
+  payments: {}
+};
 
+export function saveDB() {
+  localStorage.setItem("spirenet_db", JSON.stringify(db));
+}
+
+export function loadDB() {
+  let data = localStorage.getItem("spirenet_db");
+  if (data) {
+    Object.assign(db, JSON.parse(data));
+  }
+}
